202237dd9ac2cde9218b1ae97c16c3639e3ec859
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2023-04-05 17:36:01 -0300

    Update main.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,5 +1,5 @@
 /*Modifique o programa acima para exibir "Hello World!"
-Quatro vezes em uma linha para 6 linhas, cada uma impressa em um campo de 17 espaços.   - Neste caso usa-se a função setw();
+Quatro vezes em uma linha para 6 linhas, cada uma impressa em um campo de 17 espaÃ§os.   - Neste caso usa-se a funÃ§Ã£o setw();
 Use for-loops para fazer isso.*/
 
 #include <iostream>
@@ -9,20 +9,22 @@ using namespace std;
 
 int main()
 {
+    cout <<  std::setiosflags(ios::left);                 // este recurso alinha o texto Ã  esquerda (Flags = bandeiras)
+    
     for (size_t i=0; i<6; i++)                            // loop de seis linhas
     {
             for(size_t j=0; j<4; j++)                     // loop de quatro colunas
             {
                 //if (j!=0)
-                //    cout<<"\t\t";                      // quando o iterador chega no início da linha (j==0), não dá tab
-                //if (j==0 && i!=0)                      // ele não pula linha na primeira iteração de "i", ou seja, na linha 0
+                //    cout<<"\t\t";                      // quando o iterador chega no inÃ­cio da linha (j==0), nÃ£o dÃ¡ tab
+                //if (j==0 && i!=0)                      // ele nÃ£o pula linha na primeira iteraÃ§Ã£o de "i", ou seja, na linha 0
                 //{
                 //    cout<<endl;
                 //}
                 //cout << "Hello world!";
-                cout << setw(17) << "Hello World!";     // insere 17 espaços a cada iteração, inclusive na primeira
+                cout << setw(17) << "Hello World!";     // insere 17 espaÃ§os a cada iteraÃ§Ã£o, inclusive na primeira
             }
-            cout<<endl;                                 // no final de cada iteração, quebra uma linha
+            cout<<endl;                                 // no final de cada iteraÃ§Ã£o, quebra uma linha
     }
     return 0;
 }
